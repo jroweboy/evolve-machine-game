4231d6d09597642525b3abb8a8d4a135336711f3
James Rowe <jroweboy@gmail.com>
2023-09-10 17:01:37 -0400

    Fix more cmake crap

--- cmake/CompressCHR.cmake
+++ cmake/CompressCHR.cmake
@@ -11,32 +11,28 @@ function(compress_chr)
   if (NOT COMPRESS_CHR_DEST)
     message(FATAL_ERROR "Compressed CHR Gen DEST folder is required!")
   endif()
+
+  set(OUT ${COMPRESS_CHR_DEST}/chr)
   
   file(GLOB infiles "${COMPRESS_CHR_SRC}/*.chr")
-  file(GLOB outputfiles "${COMPRESS_CHR_SRC}/*.chr")
-  list(TRANSFORM outputfiles REPLACE "${COMPRESS_CHR_SRC}/(.*)\.chr" "${COMPRESS_CHR_DEST}/\\1\.bin")
-
+  file(GLOB outfiles "${COMPRESS_CHR_SRC}/*.chr")
+  list(TRANSFORM outfiles REPLACE "${COMPRESS_CHR_SRC}/(.*)\.chr" "${OUT}/\\1\.bin")
   find_package(Python3 REQUIRED)
 
   find_file(compressor_script NAMES donut donut.py)
   if (NOT compressor_script)
     message(FATAL_ERROR "Cannot generate compressed CHR: Unable to find compressor script donut.py")
   endif()
-  # find_file(bin2h NAMES bin2h bin2h.py)
-  # if (NOT bin2h)
-  #   message(FATAL_ERROR "Cannot generate compressed CHR: Unable to find bin2h.py")
-  # endif()
 
   add_custom_command(
-    OUTPUT ${outputfiles}
+    OUTPUT ${outfiles}
     # Compress all of the CHR files into bin files
-    COMMAND ${PYTHON_EXECUTABLE} ${compressor_script} -f ${COMPRESS_CHR_SRC} ${COMPRESS_CHR_DEST}
-    # Then run each file through bin2h
-    # COMMAND ${PYTHON_EXECUTABLE} ${bin2h} ${CMAKE_CURRENT_BINARY_DIR}/chr -o ${compressed_chr}
-    DEPENDS ${compressor_script} ${infiles} # ${bin2h} 
+    COMMAND ${Python3_EXECUTABLE} ${compressor_script} -f ${COMPRESS_CHR_SRC} ${OUT}
+    DEPENDS ${compressor_script}
+    DEPENDS ${infiles}
     COMMENT "Generating compressed CHR files into gen folder"
   )
-  add_library(CompressedCHR ${outputfiles})
+  add_library(CompressedCHR ${outfiles})
   set_target_properties(CompressedCHR PROPERTIES LINKER_LANGUAGE CXX)
   target_include_directories(CompressedCHR PUBLIC ${COMPRESS_CHR_DEST})
 endfunction()
--- cmake/GenerateCA65Binary.cmake
+++ cmake/GenerateCA65Binary.cmake
@@ -12,10 +12,12 @@ function(generate_ca65_binary)
     message(FATAL_ERROR "CA65 Build DEST folder is required!")
   endif()
 
+  set(DEST ${GEN_BINARY_DEST}/ca65)
+
   # set(ca65_artifacts ${GEN_BINARY_DEST}/ca65_build_output.h)
   # file(GLOB outputfiles "${GEN_BINARY_SRC}/*.chr")
   # list(TRANSFORM outputfiles REPLACE "${GEN_BINARY_SRC}/(.*)\.chr" "${GEN_BINARY_DEST}/\\1\.bin")
-  set(outputfiles ${GEN_BINARY_DEST}/prg8.bin ${GEN_BINARY_DEST}/prgc.bin)
+  set(outputfiles ${DEST}/prg8.bin ${DEST}/prgc.bin)
 
   find_package(Python3 REQUIRED)
   # find_file(bin2h NAMES bin2h bin2h.py)
@@ -23,19 +25,20 @@ function(generate_ca65_binary)
   #   message(FATAL_ERROR "Cannot generate ca65 output: Unable to find bin2h.py")
   # endif()
 
-  set(out ${CMAKE_CURRENT_BINARY_DIR}/ca65)
+  set(out ${CMAKE_CURRENT_BINARY_DIR}/ca65build)
 
   find_program(GENCA65BIN_CL65 cl65 REQUIRED HINTS ${GEN_BINARY_SRC})
 
   add_custom_command(
     OUTPUT ${outputfiles}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMAND ${CMAKE_COMMAND} -E make_directory ${out}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${GEN_BINARY_DEST}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${DEST}
     # build the files into a raw dump. the linker file is setup to spit out the banks into different files
     COMMAND ${GENCA65BIN_CL65} -g -m ${out}/map.txt --ld-args --dbgfile,${out}/out.dbg --no-target-lib -C ${GEN_BINARY_SRC}/llvm.cfg ${GEN_BINARY_SRC}/music.s ${GEN_BINARY_SRC}/donut.s 
     # Then run each file through bin2h
     # COMMAND ${PYTHON_EXECUTABLE} ${bin2h} ${CMAKE_CURRENT_BINARY_DIR}/ca65 -o ${ca65_artifacts}
-    COMMAND ${CMAKE_COMMAND} -E copy ${out}/prg8.bin ${out}/prgc.bin ${GEN_BINARY_DEST}
+    COMMAND ${CMAKE_COMMAND} -E copy ${out}/prg8.bin ${out}/prgc.bin ${DEST}
     DEPENDS ${compressor_script}
     DEPENDS ${GEN_BINARY_SRC}/evolve_machine.s
     DEPENDS ${GEN_BINARY_SRC}/famistudio_ca65.s

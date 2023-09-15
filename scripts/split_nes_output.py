750e7367cde968ff691defe09b88112899551d24
James Rowe <jroweboy@gmail.com>
2023-09-15 13:57:00 -0400

    Testing output from a new export

--- scripts/split_nes_output.py
+++ scripts/split_nes_output.py
@@ -3,6 +3,7 @@ import re
 import sys
 
 def main():
+    prgc_end = 0
     segments = False
     with open(f'{sys.argv[1]}/map.txt','r') as map:
         for line in map.readlines():
@@ -15,6 +16,9 @@ def main():
             if segments and line.startswith("LOWCODE"):
                 d=re.findall(r'LOWCODE \s*([\dA-Fa-f]*)\s*([\dA-Fa-f]*)\s',line)
                 prgc_start = int(d[0][0], 16)
+                # if theres no dpcm set this as a backup?
+                if prgc_end == 0:
+                    prgc_end = int(d[0][1], 16)
             if segments and line.startswith("DPCM"):
                 d=re.findall(r'DPCM \s*([\dA-Fa-f]*)\s*([\dA-Fa-f]*)\s',line)
                 prgc_end = int(d[0][1], 16)
@@ -27,7 +31,8 @@ def main():
         with open(f'{sys.argv[1]}/prg8.bin','wb') as prg8:
             prg8.write(bin[0x10 : 0x10 + prg8_len])
         with open(f'{sys.argv[1]}/prgc.bin','wb') as prgc:
-            prgc.write(bin[0xc003 + 0x10 : 0xc003 + 0x10 + prg8_len])
+            prgc.write(bin[0xc000 + 0x10 : 0xc000 + 0x10 + prg8_len])
+
 
 if __name__ == "__main__":
     main()
